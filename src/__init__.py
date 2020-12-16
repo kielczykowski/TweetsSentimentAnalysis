eea85c9661a6708bd80db8a9088ca130e89467d3
yoszimitsu <cotam07@gmail.com>
2020-12-16 22:05:20 +0100

    added searching by hashtags

--- src/__init__.py
+++ src/__init__.py
@@ -7,16 +7,28 @@ auth = tweepy.OAuthHandler(config.api_key, config.api_secret_key)
 auth.set_access_token(config.api_token, config.api_secret_token)
 
 api = tweepy.API(auth)
-tweetsArray = []
+
 
 # override tweepy.StreamListener to add logic to on_status
 class MyStreamListener(tweepy.StreamListener):
+    tweetsArray = []
 
     def on_status(self, status):
-        tweetsArray.append(status)
+        self.tweetsArray.append(status)
         print('Hashtags: ', status.entities.get('hashtags'), 'Text: ', status.text)
 
-myStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
 
-myStream.filter(track=['blacklivesmatter'])
+myStreamListener = MyStreamListener()
+# myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+# myStream.filter(track=['blacklivesmatter'])
+
+class SearchingTweetsByHashtags:
+
+    def search(self):
+        for tweet in tweepy.Cursor(api.search, q="#najman", count=100,
+                                   since="2019-12-01").items():
+            print('Hashtags: ', tweet.entities.get('hashtags'), 'Text: ', tweet.text)
+
+
+search = SearchingTweetsByHashtags()
+search.search()
