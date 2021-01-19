aede01ee8e79e09c6cf4f64003fdbf92133a91d5
edsuch21 <edsuch21@gmail.com>
2021-01-19 22:58:01 +0100

    dodaanie przymiotników połaczonych spójnikiem

--- spacy_test.py
+++ spacy_test.py
@@ -1,9 +1,18 @@
 import spacy
 
+tweet_text = "I bought big and sweet watermelons."
+tweet_tags = ["watermelons"]
+
 spacy.prefer_gpu()
 nlp = spacy.load('en_core_web_sm')
-doc = nlp("In hotel was good service and bad food.")
+doc = nlp(tweet_text)
 for token in doc:
-    if token.head.text == "service" or token.head.text == "food":
+    if token.head.text in tweet_tags:
         if token.dep_ in ["advmod", "advmod", "agent", "amod", "aux", "cop", "scubj", "det", "dobj", "goeswith", "iobj", "neg", "nn", "npadvmod", "nsubj", "nsubjpass", "num", "pobj", "poss", "preconj", "predeterminer", "prepc", "prt", "quantmod", "rcmod", "ref", "tmod", "xcomp", "xsubj"]:
-            print(token.text, token.head.text)
+            siblings = ""
+            for twin_token in doc:
+                if twin_token.head.text == token.text and twin_token.dep_ == "conj":
+                    siblings += " " + twin_token.text
+            if len(siblings) != 0:
+                siblings = siblings[1:len(siblings)]
+            print(token.text, siblings, token.head.text)
