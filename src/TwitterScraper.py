cd03a7dc00571be1b29623f274fd3cb5d67bf9ed
Michał Kiełczykowski <michal.k.kielczykowski@gmail.com>
2021-01-23 15:56:32 +0100

    Define output dictionary from twitter messages

--- src/TwitterScraper.py
+++ src/TwitterScraper.py
@@ -1,26 +1,66 @@
+#!/usr/bin/env python3
+
 import importlib
 import tweepy
+from datetime import date, timedelta
+import json
+import os
 
 class TwitterScraper:
     def __init__(self):
-        auth = tweepy.OAuthHandler(config.api_key, config.api_secret_key)
-        auth.set_access_token(config.api_token, config.api_secret_token)
+        api_key = os.environ["TWITTER_API_KEY"]
+        api_secret_key = os.environ["TWITTER_API_SECRET_KEY"]
+        api_token = os.environ["TWITTER_API_TOKEN"]
+        api_secret_token = os.environ["TWITTER_API_SECRET_TOKEN"]
+        auth = tweepy.OAuthHandler(api_key, api_secret_key)
+        auth.set_access_token(api_token,  api_secret_token)
+        self.api = tweepy.API(auth, wait_on_rate_limit=True)  # wait_on_rate_limit - waits until 15 minutes rate limit gets refreshed
 
-        self.api = tweepy.API(auth)
-
-    def search(self, phrase = "#najman", since="2019-12-01"):
-        found_tags = []
-        for tweet in tweepy.Cursor(self.api.search, phrase, count = 100, since = since).items():
-            print('Hashtags: ', tweet.entities.get('hashtags'), 'Text: ', tweet.text)
+    def search(self, phrase="#fitness", since="2021-01-22"
+            , until=(date.today() + timedelta(days=1)).strftime("%Y-%m-%d")  # until today excludes todays'tweets
+            , tweets_number=10, tweet_mode='extended', language='pl'):
+        found_tweets = []
+        for tweet in tweepy.Cursor(
+            self.api.search
+            , phrase
+            , since = since
+            , until=until, lang=language
+            , tweet_mode=tweet_mode).items(tweets_number):
+            # print('Hashtags: ', tweet.entities.get('hashtags'), '\nText: ', tweet.full_text,
+            # '\nLang: ', tweet.lang, '\nMetadata: ', tweet.metadata)
+            # print('\nNowy tweet:\n')
+            # print('Hashtags: ', tweet.entities.get('hashtags'))
+            # print('Created_at: ', tweet.created_at, type(tweet.created_at))
+            # print('id: ', tweet.id, type(tweet.id))
+            # print('place: ', tweet.place, type(tweet.place))
+            # print('\n DIR: ', dir(tweet))
             temp = {}
-            temp["hashtags"] = tweet.entities.get('hashtags')
-            temp["contents"] = tweet.text
-            found_tags.append(temp)
-        self.found_tags = found_tags
-        return found_tags
+            temp["twitter"] = {}
+            if 'retweeted_status' in dir(tweet):
+                temp["text"] = tweet.retweeted_status.full_text
+            else:
+                temp["text"] = tweet.full_text
+
+            temp["id"] = tweet.id
+            temp["query"] = phrase
+            temp["creationTime"] = str(tweet.created_at)
+            temp["twitter"]["detectedLanguage"] = tweet.lang
+            temp["twitter"]["hashtags"] = tweet.entities.get('hashtags')
+            if tweet.place is not None:
+                temp["twitter"]["place"] = {
+                    key:getattr(tweet.place, key) for key in ['place_type', 'name', 'full_name', 'country']
+                }
+                temp["twitter"]["place"]["coordinatesType"] = tweet.place.bounding_box.type
+                temp["twitter"]["place"]["coordinates"] = tweet.place.bounding_box.coordinates
+
+            found_tweets.append(temp)
+        self.found_tweets = found_tweets
+        return found_tweets
 
 
 if __name__ == "__main__":
     search = TwitterScraper()
     tweets = search.search()
-    print(len(tweets))
+    print("\nreturned object\n")
+    for element in tweets:
+        print(element)
