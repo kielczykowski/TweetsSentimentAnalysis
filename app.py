972bd10373a225fe0270ad23c056b31215653e52
kkorniszuk <korniszuk.krzysztof@gmail.com>
2021-01-27 23:24:42 +0100

    Set body of request to json

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
 from flask import Flask, request
 from flask_restful import Api, Resource, reqparse
-
 app = Flask(__name__)
 api = Api(app)
 
 hashtag_args = reqparse.RequestParser()
-hashtag_args.add_argument("hashtag", type=str, help="'hashtag' param is required.", required=True)
-hashtag_args.add_argument("date", type=str, help="'date' param is required.", required=True)
-hashtag_args.add_argument("limit", type=int, help="'limit' param is required.", required=True)
-hashtag_args.add_argument("language", type=str, help="'lang' param is required.", required=True)
-
 
 class HashtagRestAPi(Resource):
-    def get(self):
-        hashtag_args.parse_args()
-        hashtag = request.args['hashtag']
-        limit = request.args['limit']
-        date = request.args['date']
-        lang = request.args['language']
+    def post(self):
+        payload = request.json
+        hashtag = payload['hashtag']
+        limit = payload['limit']
+        date = payload['fromDate']
+        lang = payload['language']
         #
         #
         # Processing...
